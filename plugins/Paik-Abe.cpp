2984e50c9f127226323fb7f5edefa46331a4593e
Michael Dewberry <michael.dewberry@autodesk.com>
2014-03-08 20:58:36 -0500

    Paik-Abe achieves minimum viable art

--- plugins/Paik-Abe.cpp
+++ plugins/Paik-Abe.cpp
@@ -1,8 +1,8 @@
 //
-//  Blockhead.cpp
+//  Paik-Abe.cpp
 //  GrainmarchFFGL
 //
-//  Created by Michael Dewberry on 11/24/13.
+//  Created by Michael Dewberry on 3/7/14.
 //
 //
 
@@ -18,18 +18,38 @@
 // Parameter(uniform name, minimum value, maximum value, default value, optional type (default standard/float.))
 
 BEGIN_SHADER_PARAMETERS()
-PARAM(Time, 0.0, 1.0, 0.0, FF_TYPE_STANDARD, false)
 PARAM(FrameRate, 0.0, 60.0, 30.0, FF_TYPE_STANDARD, false)
 PARAM(Scanlines, 0.0, 1000.0, 525.0, FF_TYPE_STANDARD, false)
 PARAM(SamplesPerFrame, 0.0, 100000.0, 20000.0, FF_TYPE_STANDARD, false)
+PARAM(StandardYokeX, -2.0, 2.0, 1.0, FF_TYPE_STANDARD, false)
+PARAM(StandardYokeY, -2.0, 2.0, 1.0, FF_TYPE_STANDARD, false)
+PARAM(SineFreq, 0.0, 1000.0, 1.0, FF_TYPE_STANDARD, false)
+PARAM(CosFreq, 0.0, 1000.0, 1.0, FF_TYPE_STANDARD, false)
+PARAM(TriFreq, 0.0, 1000.0, 1.0, FF_TYPE_STANDARD, false)
+PARAM(SineAmpX, -2.0, 2.0, 0.0, FF_TYPE_STANDARD, false)
+PARAM(SineAmpY, -2.0, 2.0, 0.0, FF_TYPE_STANDARD, false)
+PARAM(SineFreqX, -3.0, 3.0, 0.0, FF_TYPE_STANDARD, false)
+PARAM(SineFreqY, -3.0, 3.0, 0.0, FF_TYPE_STANDARD, false)
+PARAM(CosAmpX, -2.0, 2.0, 0.0, FF_TYPE_STANDARD, false)
+PARAM(CosAmpY, -2.0, 2.0, 0.0, FF_TYPE_STANDARD, false)
+PARAM(CosFreqX, -3.0, 3.0, 0.0, FF_TYPE_STANDARD, false)
+PARAM(CosFreqY, -3.0, 3.0, 0.0, FF_TYPE_STANDARD, false)
+PARAM(TriAmpX, -2.0, 2.0, 0.0, FF_TYPE_STANDARD, false)
+PARAM(TriAmpY, -2.0, 2.0, 0.0, FF_TYPE_STANDARD, false)
+PARAM(TriFreqX, -3.0, 3.0, 0.0, FF_TYPE_STANDARD, false)
+PARAM(TriFreqY, -3.0, 3.0, 0.0, FF_TYPE_STANDARD, false)
+PARAM(DampingX, 0.0, 20.0, 0.0, FF_TYPE_STANDARD, false)
+PARAM(DampingY, 0.0, 20.0, 0.0, FF_TYPE_STANDARD, false)
+
 END_SHADER_PARAMETERS()
 
-class PaikAbePlugin : public SourcePlugin
+class PaikAbePlugin : public EffectPlugin
 {
 public:
     PaikAbePlugin() {}
     class Point {
     public:
+        Point() : x(0), y(0) {};
         Point(float xx, float yy) : x(xx), y(yy) {};
         float x;
         float y;
@@ -56,17 +76,23 @@ public:
     }
      */
     
-    void drawLine(float x1, float y1, float x2, float y2, float lineWidth) {
-        glVertex2f((2.0 * x1) - 1.0, (2.0 * y1) - 1.0);
-        glVertex2f((2.0 * x2) - 1.0, (2.0 * y2) - 1.0);
+    void drawLine(Point a, Point b, Point ta, Point tb, float lineWidth) {
+        glMultiTexCoord2f(0, (ta.x + 1) / 2, (ta.y + 1) / 2);
+        glVertex2f(a.x, a.y);
+        glMultiTexCoord2f(0, (tb.x + 1) / 2, (tb.y + 1) / 2);
+        glVertex2f(b.x, b.y);
     }
     
     double horizontalBeamFunction(double t, double linePeriod) {
-        return fmod(t, linePeriod) / linePeriod;
+        return 2.0 * fmod(t, linePeriod) / linePeriod - 1.0;
     }
     
     double verticalBeamFunction(double t, double linePeriod, double lines) {
-        return floor(t / linePeriod) / lines;
+        return 2.0 * floor(t / linePeriod) / lines - 1.0;
+    }
+    
+    double tri(double t) {
+        return 2.0 * fabs(2.0 * (t - floor(t + 0.5))) - 1.0;
     }
     
     virtual void EmitGeometry() {
@@ -91,8 +117,6 @@ public:
         glMatrixMode(GL_MODELVIEW);
         glLoadIdentity();
         
-        beamPath.clear();
-        
         double linesPerSecond = GetScaled(Param::FrameRate) * GetScaled(Param::Scanlines);
         double linePeriod = 1.0 / linesPerSecond;
         double framePeriod = 1.0 / GetScaled(Param::FrameRate);
@@ -101,14 +125,33 @@ public:
         double t = 0;
         glBegin(GL_LINES);
         glLineWidth(3.0);
-        Point lastPoint(0, 0);
-        Point currentPoint = lastPoint;
+        Point lastPoint, currentPoint, lastSourcePoint, currentSourcePoint;
+        
         while (t < framePeriod)
         {
+            double h = horizontalBeamFunction(t, linePeriod);
+            double v = verticalBeamFunction(t, linePeriod, GetScaled(Param::Scanlines));
+            
+            double x = (GetScaled(Param::StandardYokeX) * h +
+                        GetScaled(Param::SineAmpX) * sin(t * GetScaled(Param::SineFreq) * pow(2.0, GetScaled(Param::SineFreqX))) +
+                        GetScaled(Param::CosAmpX) * cos(t * GetScaled(Param::CosFreq) * pow(2.0, GetScaled(Param::CosFreqX))) +
+                        GetScaled(Param::TriAmpX) * tri(t * GetScaled(Param::TriFreq) * pow(2.0, GetScaled(Param::TriFreqX)))) *
+                        exp(-1.0 * GetScaled(Param::DampingX) * t);
+            double y = (GetScaled(Param::StandardYokeY) * v +
+                        GetScaled(Param::SineAmpY) * sin(t * GetScaled(Param::SineFreq) * pow(2.0, GetScaled(Param::SineFreqY))) +
+                        GetScaled(Param::CosAmpY) * cos(t * GetScaled(Param::CosFreq) * pow(2.0, GetScaled(Param::CosFreqY))) +
+                        GetScaled(Param::TriAmpY) * sin(t * GetScaled(Param::TriFreq) * pow(2.0, GetScaled(Param::TriFreqY)))) *
+                        exp(-1.0 * GetScaled(Param::DampingY) * t);
+
+            currentPoint = Point(x, y);
+            currentSourcePoint = Point(h, v);
+            
+            if (t != 0.0)
+                drawLine(lastPoint, currentPoint, lastSourcePoint, currentSourcePoint, 2.0);
+
             lastPoint = currentPoint;
-            currentPoint = Point(horizontalBeamFunction(t, linePeriod),
-                                 verticalBeamFunction(t, linePeriod, GetScaled(Param::Scanlines)));
-            drawLine(lastPoint.x, lastPoint.y, currentPoint.x, currentPoint.y, 2.0);
+            lastSourcePoint = currentSourcePoint;
+
             t += advance;
         }
         glEnd();
--- plugins/Paik-Abe.glsl
+++ plugins/Paik-Abe.glsl
@@ -4,7 +4,6 @@ void main()
 {    
     gl_Position = gl_ProjectionMatrix * gl_ModelViewMatrix * gl_Vertex;
     gl_TexCoord[0] = gl_MultiTexCoord0;
-    gl_TexCoord[1] = gl_MultiTexCoord1;
 }
 
 
@@ -13,12 +12,12 @@ void main()
 char fragmentShaderCode[] = R"(
 
 uniform sampler2D inputTexture0;
-uniform float Border;
 
 void main()
 {
     vec2 texCoords = gl_TexCoord[0].st;
-    gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
+    gl_FragColor = texture2D(inputTexture0, texCoords);
+    gl_FragColor.a = 1.0;
 }
 
 )";
