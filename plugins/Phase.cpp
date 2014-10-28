d9bdc8e29f41a17d94ec1239876bbbd02710ae5b
Michael Dewberry <michael.dewberry@autodesk.com>
2014-10-27 23:33:00 -0400

    Complete basic phase portrait implementation

--- plugins/Phase.cpp
+++ plugins/Phase.cpp
@@ -11,15 +11,35 @@
 #include "default_vertex_shader.glsl"
 #include <cmath>
 
+#define PI 3.1415926535897932384626433832795
+
 DECLARE_PLUGIN(SourcePlugin, "DBPH", "Phase", "Complex phase portrait experiment", "by Michael Dewberry - dewb.org")
 
 // Parameter(uniform name, minimum value, maximum value, default value, optional type (default standard/float.))
 
 BEGIN_SHADER_PARAMETERS()
-PARAM(Domain, 0.1, 100.0, 1.2)
-PARAM(w_x, -1.0, 1.0, 0.30902)
-PARAM(w_y, -1.0, 1.0, 0.95106)
-PARAM(t, pow(10, -8), pow(10, -4), pow(10, -8))
+PARAM(Domain, 0.1, 40.0, 1.2)
+PARAM(Direction, 0, 2 * PI, 0)
+PARAM(Error, 0, 1.0, 0)
+PARAM(A, -100, 100.0, 1)
+PARAM(B, -100, 100.0, -21)
+PARAM(C, -100, 100.0, 35)
+PARAM(D, -100, 100.0, -7)
+PARAM(E, -100, 100.0, -6)
+PARAM(F, -100, 100.0, 1)
+PARAM(G, -100, 100.0, 1)
+PARAM(H, -100, 100.0, -7)
+PARAM(I, -100, 100.0, 35)
+PARAM(J, -100, 100.0, 4)
+PARAM(K, -100, 100.0, -21)
+PARAM(L, -100, 100.0, -4)
+PARAM(M, -100, 100.0, 1)
+PARAM(N, -100, 100.0, 2)
+PARAM(ColorMode, 0, 1, 0);
+PARAM(HueLimit, 0, 1, 1);
+PARAM(HueOffset, 0, 1, 0);
+PARAM(Saturation, 0, 1, 1);
+PARAM(Function, 0, 1, 0);
 END_SHADER_PARAMETERS()
 
 
--- plugins/Phase.glsl
+++ plugins/Phase.glsl
@@ -4,26 +4,107 @@ uniform vec3      iResolution;           // viewport resolution (in pixels)
 uniform float     iGlobalTime;           // shader playback time (in seconds)
 uniform sampler2D inputTexture0;
 
+#define PI 3.1415926535897932384626433832795
+
 uniform float Domain;
-uniform float w_x;
-uniform float w_y;
-uniform float t;
+uniform float Direction;
+uniform float Error;
+uniform float ColorMode;
+uniform float HueLimit;
+uniform float HueOffset;
+uniform float Saturation;
+uniform float Function;
+
+uniform float A; // 1
+uniform float B; // 21
+uniform float C; // 35
+uniform float D; // 7
+uniform float E; // 6
+uniform float F; // 1
+uniform float G; // 1
+
+uniform float H; // -7
+uniform float I; // 35
+uniform float J; // 4
+uniform float K; // 21
+uniform float L; // 4
+uniform float M; // 1
+uniform float N; // 2
+
+
+// From https://github.com/hughsk/glsl-hsv2rgb
+
+vec3 hsv2rgb(vec3 c) {
+    vec4 K = vec4(1.0, 2.0 / 3.0, 1.0 / 3.0, 3.0);
+    vec3 p = abs(fract(c.xxx + K.xyz) * 6.0 - K.www);
+    return c.z * mix(K.xxx, clamp(p - K.xxx, 0.0, 1.0), c.y);
+}
 
 vec2 f(vec2 z) {
     float x = z.x;
+    float x2 = x * x;
+    float x3 = x * x2;
+    float x4 = x * x3;
+    float x5 = x * x4;
+    float x6 = x * x5;
+    float x7 = x * x6;
+    
     float y = z.y;
-    return vec2(x * x + x * y, x * x - y * y);
+    float y2 = y * y;
+    float y3 = y * y2;
+    float y4 = y * y3;
+    float y5 = y * y4;
+    float y6 = y * y5;
+    float y7 = y * y6;
+
+    if (Function < 0.5) {
+        return vec2(A * x7     + B * x5 * y2 + C * x3 * y4 + D * x * y7  + E * x2 * y2 + F * y4 + G,
+                    H * x6 * y + I * x4 * y3 + J * x3 * y  + K * x2 * y5 + L * x * y3  + M * y7 + N * y) +
+         vec2(x * x + x * y, x * x - y * y);
+    } else {
+        return vec2(x * x + x * y, x * x - y * y);
+    }
+}
+
+vec4 phasePortraitColor(float phase) {
+    vec4 color;
+    float phaseScaled = phase / (2.0 * PI);
+    float hue = mod(phaseScaled * HueLimit + HueOffset, 1.0);
+    float v = 1.0 - (1.0 - Saturation) * phaseScaled;
+    color.rgb = hsv2rgb(vec3(hue, Saturation, v));
+    color.a = 1.0;
+    return color;
 }
 
 void main(void)
 {
-    vec2 w = vec2(w_x, w_y);
+    vec2 w = vec2(cos(Direction), sin(Direction));
+    float t = Domain * pow(10.0, -Error * 9.0 - 2.0);
+    
     vec2 z0 = (gl_TexCoord[0].st - 0.5) * 2.0 * Domain;
 	vec2 fz0 = f(z0);
     vec2 fzd = f(z0 + t * w);
     vec2 fk = (fzd - fz0) / (t * w);
     
-	gl_FragColor = vec4(fk.x, fk.y, 0.0, 1.0);
+    float phase = atan(fk.y, fk.x);
+    if (phase < 0.0) {
+        phase += 2.0 * PI;
+    }
+    
+    if ((Direction > PI/2.0 && Direction < PI)|| (Direction > 3.0*PI/2.0)) {
+        phase = mod(phase + PI, 2.0 * PI);
+        fk = -fk;
+    }
+    
+    if (ColorMode < 0.25) {
+        gl_FragColor = phasePortraitColor(phase);
+    } else if (ColorMode < 0.5) {
+        gl_FragColor = vec4(fk.x, fk.y, 0.0, 1.0);
+    } else if (ColorMode < 0.75) {
+        gl_FragColor = vec4(fk.x, 0, fk.y, 1.0);
+    } else {
+        gl_FragColor = vec4(0, fk.x, fk.y, 1.0);
+    }
 }
 
 
